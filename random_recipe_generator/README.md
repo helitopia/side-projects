5fa08c085ed932e29a87a5775e637bb5fba3d38b
Illia Harkusha <illia.harkusha.w@gmail.com>
2024-06-13 20:47:39 +0300

    random_recipe_generator: dynamic recipe generation & display implemented (-ingredients list), TODO updated

--- random_recipe_generator/README.md
+++ random_recipe_generator/README.md
@@ -1,6 +1,6 @@
 # Random recipe generator
 
 ## TODO
-- Add arrow-button to get back to generation page
 - Implement recipe API fetching and corresponding page parts display 
 - Add categorization of generated recipes (e.g. Chinese, Indian, Seafood)
+- Bug: When text content is quite large, the image is stretched looking ugly
--- random_recipe_generator/src/script.js
+++ random_recipe_generator/src/script.js
@@ -1,11 +1,55 @@
-const generateRecipeBtn = document.querySelector("#generate-recipe");
-const generatedRecipeContainer = document.querySelector("#generated-recipe-container")
-const suggestRecipeContainer = document.querySelector("#suggest-recipe-container")
+const recipeImage = document.querySelector("#recipe-image");
+const recipeName = document.querySelector("#recipe-name");
+const recipeInstructions = document.querySelector("#recipe-instructions");
+const recipeIngredients = document.querySelector("#recipe-ingredients");
+const recipeVideo = document.querySelector("#video");
 
-generateRecipeBtn.addEventListener("click", () => generateRecipe())
+const apiPropertyMapping = {
+    IMAGE_THUMBNAIL: "strMealThumb",
+    RECIPE_NAME: "strMeal",
+    RECIPE_INSTRUCTIONS: "strInstructions",
+    VIDEO_SOURCE: "strYoutube",
+    INGREDIENT_WILDCARD: "strIngredient{N}",
+    MEASURE_WILDCARD: "strMeasure{N}"
+};
+
+class Recipe {
+    constructor(apiJson) {
+        console.log(apiJson)
+        this.img = apiJson[apiPropertyMapping.IMAGE_THUMBNAIL];
+        this.name = apiJson[apiPropertyMapping.RECIPE_NAME];
+        this.instructions = apiJson[apiPropertyMapping.RECIPE_INSTRUCTIONS];
+        this.ingredients = Recipe.parseIngredientsAndMeasures(apiJson)
+        this.videoSource = apiJson[apiPropertyMapping.VIDEO_SOURCE];
+    }
+
+    static parseIngredientsAndMeasures(api){
+        return ["a", "b", "c"] //demo
+    }
+}
 
 function generateRecipe() {
-    suggestRecipeContainer.style.display = "none";
-    generatedRecipeContainer.style.display = "block";
-    //TODO: Fetch and display recipe information
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(r => r.json())
+        .then(r => new Recipe(r.meals[0]))
+        .then(displayRecipe);
 }
+
+function displayRecipe(recipe) {
+    recipeImage.src = recipe.img;
+    recipeName.textContent = recipe.name;
+    recipeInstructions.textContent = recipe.instructions;
+    getHtmlList(recipe.ingredients)
+        .forEach(r => recipeIngredients.appendChild(r));
+    recipeVideo.src = `https://www.youtube.com/embed/${recipe.videoSource.slice(-11)}`;
+}
+
+function getHtmlList(ingredients) {
+    return ingredients.map(i => {
+        let elem = document.createElement("li")
+        elem.innerHTML = i;
+        return elem;
+    });
+}
+
+generateRecipe();
