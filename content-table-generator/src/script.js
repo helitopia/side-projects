e2374a5241a8c8ed4e58ded700ef5eea45a30726
harkusha <illia.harkusha.w@gmail.com>
2024-07-18 20:26:20 +0300

    content-table-generator #38: Headings numbering implemented

--- content-table-generator/src/script.js
+++ content-table-generator/src/script.js
@@ -1,1 +1,20 @@
 alert("The project is \"work in progress\"");
+
+const headingTags = ["h2", "h3", "h4", "h5", "h6"];
+let headings = document.querySelectorAll(headingTags.join(",")),
+    h2Count = 0,
+    headingNums,
+    hPrevLevel = -1;
+
+for (let heading of headings) {
+    let hLevel = heading.nodeName[1];
+    if (hLevel === "2")
+        headingNums = [h2Count++, 0, 0, 0, 0];
+
+    if (hLevel < hPrevLevel)
+        headingNums[hPrevLevel - 2] = 0;
+
+    hPrevLevel = hLevel;
+    headingNums[hLevel - 2]++;
+    heading.prepend(headingNums.slice(0, hLevel - 1).join(".") + ". ");
+}
